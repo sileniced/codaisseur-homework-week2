fdfbc38499e36a94428f76c97dec6b70c2afdc63
Vince Liem <vincepaul.liem@gmail.com>
2018-09-22 19:16:40 +0200

    shortened addSomePromises

--- src/async-functions.js
+++ src/async-functions.js
@@ -3,9 +3,9 @@ const giveItBackLater = (value, callback) => setTimeout(() => callback(value), 1
 const promiseToGiveItBackLater = value => new Promise(resolve => setTimeout(() => resolve(value), 100));
 
 const addSomePromises = somePromise => new Promise(resolve =>
-  somePromise
-  .then(res => resolve(res + res))
-  .catch(err => resolve(err.repeat(3)))
+  resolve(somePromise
+  .then(res => res + res)
+  .catch(err => err + err + err))
 );
 
 module.exports = {
